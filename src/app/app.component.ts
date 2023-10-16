3f236f42e3debcbaf29cc7ba7db1d0215aa5cb39
Sergiyan92 <sergiyan@ukr.net>
2023-10-16 12:15:06 +0300

    api services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import { products as data } from './data/products';
+import { ProductServises } from './services/products.services';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
-  products: IProduct[] = data;
+  // products: IProduct[] = [];
+  loading = false;
+  products$: Observable<IProduct[]>;
+
+  constructor(private productServises: ProductServises) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productServises
+      .getAll()
+      .pipe(tap(() => (this.loading = false)));
+    // this.productServises.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
+  }
 }
